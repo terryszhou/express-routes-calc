d56721fe5f72531832ece64b4574d27b12bcacbd
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-05-27 17:56:29 -0500

    Added notes for clarity.

--- index.js
+++ index.js
@@ -7,45 +7,46 @@ app.get("/", (req, res) => {
     res.send("Hello, world!")
 })
 
-// app.get("/:z/:x/:y", (req, res) => {
-//     const z = req.params.z
-//     const x = parseInt(req.params.x)
-//     const y = parseInt(req.params.y)
+//FULL SOLVE (one function)
+app.get("/:z/:x/:y", (req, res) => {
+    const z = req.params.z
+    const x = parseInt(req.params.x)
+    const y = parseInt(req.params.y)
 
-//     if (z == "add") {
-//         res.send("<h1>" + `${x} + ${y} = ${x + y}.` + "</h1>")
-//     } else if (z == "subtract") {
-//         res.send("<h1>" + `${x} - ${y} = ${x - y}.` + "</h1>")
-//     } else if (z == "multiply") {
-//         res.send("<h1>" + `${x} x ${y} = ${x * y}.` + "</h1>")
-//     } else if (z == "divide") {
-//         res.send("<h1>" + `${x} / ${y} = ${x / y}.` + "</h1>")
-//     } else {
-//         res.send("<h1>wut</h1>")
-//     }
-// })
-
-
-app.get("/*", (req, res) => {
-    const pms = req.params[0].split("/")
-    const math = req.query.math
-
-    if (math == "add") {
-        res.send(pms.reduce((a , b) => (a + b)))
-    } else if (math == "subtract") {
-        res.send(pms.reduce((a , b) => (a - b)))
-    } else if (math == "multiply") {
-        res.send(pms.reduce((a , b) => (a * b)))
-    } else if (math == "divide") {
-        res.send(pms.reduce((a , b) => (a / b)))
-    } 
+    if (z == "add") {
+        res.send("<h1>" + `${x} + ${y} = ${x + y}.` + "</h1>")
+    } else if (z == "subtract") {
+        res.send("<h1>" + `${x} - ${y} = ${x - y}.` + "</h1>")
+    } else if (z == "multiply") {
+        res.send("<h1>" + `${x} x ${y} = ${x * y}.` + "</h1>")
+    } else if (z == "divide") {
+        res.send("<h1>" + `${x} / ${y} = ${x / y}.` + "</h1>")
+    } else {
+        res.send("<h1>wut</h1>")
+    }
 })
+
+// UNFINISHED WILDCARD SOLVE
+// app.get("/*", (req, res) => {
+//     const pms = req.params[0].split("/")
+//     const math = req.query.math
+
+//     if (math == "add") {
+//         res.send(pms.reduce((a , b) => (a + b)))
+//     } else if (math == "subtract") {
+//         res.send(pms.reduce((a , b) => (a - b)))
+//     } else if (math == "multiply") {
+//         res.send(pms.reduce((a , b) => (a * b)))
+//     } else if (math == "divide") {
+//         res.send(pms.reduce((a , b) => (a / b)))
+//     } 
+// })
 
 app.listen(PORT, () => {
     log(`The port level! IT'S OVER ${PORT}!!!`)
 })
 
-// WORKING SOLUTION
+// WORKING SOLUTION -- alternate
 // app.get("/:x/:y", (req, res) => {
 //     const x = parseInt(req.params.x)
 //     const y = parseInt(req.params.y)
